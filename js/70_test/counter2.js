ad4336176c800f96c1c16d0ad710d04fa0e38762
HeshFisher <thelearningbboy@gmail.com>
2025-09-28 19:24:49 -0400

    test

---
+++ js/70_test/counter2.js
@@ -0,0 +1,24 @@
+window.app = window.app||{};
+
+window.app.counter2 = (function (module) {
+  'use strict';
+  let created = 1;
+  module.createCounter = function createCounter() {
+    let count = 0;
+    return{
+      increment(){
+        return count++;
+      },
+      getCount(){
+        return count;
+      },
+      amountCreated(){
+       return console.log(`you have created ${created++} counters`);
+      }
+    };
+  };
+  return module;
+}(window.app.counter2||{}));
+
+
+
